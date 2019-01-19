06aeaea4054c2537fd22ce2d968b711b42fb1088
Viktors Jenovs <jviktors@gmail.com>
2019-01-19 20:57:57 +0200

    Update server url

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,5 +2,5 @@ const production = process.env.NODE_ENV === 'production';
 
 // eslint-disable-next-line
 export const BASE_URL = production
-  ? 'https://9djqi1jjdc.execute-api.us-east-1.amazonaws.com/dev/sales'
+  ? 'https://yy2d3kf4y4.execute-api.us-east-1.amazonaws.com/dev/sales'
   : 'http://localhost:3001/sales';
