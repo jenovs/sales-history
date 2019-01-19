1bd14f0c989c49029d36ae9a9a28ffe931ea2ec6
Viktors Jenovs <jviktors@gmail.com>
2019-01-19 21:03:52 +0200

    Change server url to eu-central-1

--- src/config/urls.js
+++ src/config/urls.js
@@ -2,5 +2,5 @@ const production = process.env.NODE_ENV === 'production';
 
 // eslint-disable-next-line
 export const BASE_URL = production
-  ? 'https://yy2d3kf4y4.execute-api.us-east-1.amazonaws.com/dev/sales'
+  ? 'https://8mmh925wuk.execute-api.eu-central-1.amazonaws.com/dev'
   : 'http://localhost:3001/sales';
