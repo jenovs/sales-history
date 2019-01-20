ff565be7e7c8fecde68dffe6558c0a169df5955a
Viktors Jenovs <jviktors@gmail.com>
2019-01-20 16:37:18 +0200

    Remove registerServiceWorker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import registerServiceWorker from './registerServiceWorker';
 
 import './reset.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
