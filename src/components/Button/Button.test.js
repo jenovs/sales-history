cafa5cf11149499551b11c4abed9ec8dfe0e21e7
Viktors Jenovs <jviktors@gmail.com>
2019-01-20 14:31:47 +0200

    Add tests for Button component

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { render, cleanup } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
+import renderer from 'react-test-renderer';
 import 'jest-dom/extend-expect';
 
 import Button from '.';
@@ -7,14 +8,44 @@ import Button from '.';
 describe('Button', () => {
   afterEach(cleanup);
 
+  const mockClick = jest.fn();
+
   const props = {
-    amount: 123,
-    paidBy: 'card',
-    status: 'successful',
-    timestamp: 1547766061000,
+    onClick: () => {},
   };
 
-  it('should render a successful transaction', () => {
-    // todo
+  it('should render a button with a label', () => {
+    const { getByText } = render(<Button {...props}>Foo</Button>);
+
+    expect(getByText('Foo')).toBeVisible();
+  });
+
+  it('should register a click', () => {
+    const { getByText } = render(
+      <Button {...props} onClick={mockClick}>
+        Foo
+      </Button>
+    );
+
+    fireEvent.click(getByText('Foo'));
+    expect(mockClick).toHaveBeenCalled();
+  });
+
+  it('should render a dark button', () => {
+    const tree = renderer.create(<Button {...props}>Foo</Button>).toJSON();
+
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render a light button', () => {
+    const tree = renderer
+      .create(
+        <Button {...props} inactive>
+          Foo
+        </Button>
+      )
+      .toJSON();
+
+    expect(tree).toMatchSnapshot();
   });
 });
