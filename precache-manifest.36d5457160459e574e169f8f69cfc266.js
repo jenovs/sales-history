ed7f816b2dff5214d275d91553a7d9ff363b56e5
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-12-16 04:16:02 +0000

    Deploy jenovs/sales-history to github.com/jenovs/sales-history.git:gh-pages

---
+++ precache-manifest.36d5457160459e574e169f8f69cfc266.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e3d2317bf636215a40c5f19cc38f002",
+    "url": "/sales-history/index.html"
+  },
+  {
+    "revision": "9e0e020290e3f215b4c3",
+    "url": "/sales-history/static/css/main.529cb7ba.chunk.css"
+  },
+  {
+    "revision": "4c2a9b2d28291112f90a",
+    "url": "/sales-history/static/js/2.f00fb417.chunk.js"
+  },
+  {
+    "revision": "9e0e020290e3f215b4c3",
+    "url": "/sales-history/static/js/main.bf5935fc.chunk.js"
+  },
+  {
+    "revision": "02e3939369ede69a6692",
+    "url": "/sales-history/static/js/runtime-main.1ef08ab3.js"
+  },
+  {
+    "revision": "58e347fe30bcb978cd2e06fe9abe49ed",
+    "url": "/sales-history/static/media/credit-card.58e347fe.svg"
+  },
+  {
+    "revision": "d7285f3e33b1b28f2b1906148998a115",
+    "url": "/sales-history/static/media/money.d7285f3e.svg"
+  }
+]);
